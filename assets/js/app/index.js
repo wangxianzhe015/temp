96c234124e4e960f10c002199794f624995e2dfb
wangxianzhe015 <xianzhe_wang@yahoo.com>
2018-05-23 16:00:56 +0900

    fix

--- assets/js/app/index.js
+++ assets/js/app/index.js
@@ -17,6 +17,38 @@ $(document).ready(function(){
         }
     });
 
+    $('.dropzone').dropper({
+        action: "process.php"
+    }).on({
+        "start.dropper": function(e, files){
+
+        },
+        "fileComplete.dropper": function(e, file, response){
+            var $obj = $(e.target).find(".upload-check");
+            $obj.fadeIn("fast");
+            setTimeout(function () {
+                $obj.fadeOut();
+            }, 3000);
+        },
+        mouseover: function(){
+            $(this).find(".button").fadeIn("fast");
+        },
+        mouseleave: function(){
+            $(this).find(".button").fadeOut("fast");
+        }
+    }).find(".button").on("click", function(){
+        $(this).parent().find(".dropper-dropzone").click();
+    });
+
+    window.addEventListener("dragover",function(e){
+        e = e || event;
+        e.preventDefault();
+    },false);
+    window.addEventListener("drop",function(e){
+        e = e || event;
+        e.preventDefault();
+    },false);
+
     $("#plus-button").on("click", function(){
         loadIframe();
     });
@@ -28,16 +60,24 @@ $(document).ready(function(){
 
     $("#save-confirm").on("click", function(){
         var newName = $("#save-file-name").val().trim();
+        var oldName;
+
+        if ($targetButton.data("renamed")){
+            oldName = $targetButton.text() + "--" + $targetButton.data("time");
+        } else {
+            oldName = "--" + $targetButton.data("time");
+        }
+
         $.ajax({
             url: "process.php",
             type: "POST",
             data: {
                 action: "rename-app",
-                oldName: $targetButton.text() + "-" + $targetButton.data("time"),
-                newName: newName + "-" + $targetButton.data("time")
+                oldName: oldName,
+                newName: newName + "--" + $targetButton.data("time")
             },
             success: function(){
-                $targetButton.text(newName);
+                $targetButton.text(newName).data("renamed", true);
                 $("#change-name").fadeOut();
                 $("#save-file-name").val("");
             }
@@ -72,20 +112,29 @@ $(document).ready(function(){
     });
 });
 
-function addButton(text){
+function addButton(name){
+    var text = name.split("--")[0].trim();
+
     $("<button></button>", {
-        text: text.split("-")[0],
+        text: text==""?timeConverter(name.split("--")[1]):text,
         class: "normal-btn app-btn"
     }).data({
-        time: text.split("-")[1]
+        time: name.split("--")[1],
+        renamed: text!=""
     }).on({
         click: function(){
-            loadIframe($(this).text() + "-" + $(this).data("time"));
+            var name;
+            if ($(this).data("renamed")){
+                name = $(this).text() + "--" + $(this).data("time");
+            } else {
+                name = "--" + $(this).data("time");
+            }
+            loadIframe(name, $(this).text());
         }
     }).appendTo("#buttons");
 }
 
-function loadIframe(name){
+function loadIframe(name, text){
     if (name == undefined){
         name = "";
     }
@@ -93,7 +142,7 @@ function loadIframe(name){
     var iframeContainer = $("#iframe-container"), iframe = iframeContainer.find("iframe"), frameWin = iframe[0].contentWindow;
     iframe.attr("src", window.location.href + "/iframe/").off("load").on({
         load: function(){
-            frameWin.postMessage({action: 'app-name', name: name}, '*');
+            frameWin.postMessage({action: 'app-name', name: name, text: text}, '*');
         }
     });
 
@@ -109,8 +158,26 @@ function parentMessageHandler(message){
                 break;
             }
             if (message.data.isNew) {
-                addButton("data-" + message.data.time);
+                addButton("--" + message.data.time);
             }
             break;
     }
+}
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    //var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = (a.getMonth() + 1).toString();
+    month = month.length == 1? "0" + month: month;
+    //var month = months[a.getMonth()];
+    var date = a.getDate().toString();
+    date = date.length == 1? "0" + date: date;
+    var hour = a.getHours().toString();
+    hour = hour.length == 1? "0" + hour: hour;
+    var min = a.getMinutes().toString();
+    min = min.length == 1? "0" + min: min;
+    var sec = a.getSeconds().toString();
+    sec = sec.length == 1? "0" + sec: sec;
+    return year + '-' + month + '-' + date + ' ' + hour + ':' + min + ':' + sec ;
 }
--- iframe/iframe.js
+++ iframe/iframe.js
@@ -1,12 +1,76 @@
 window.addEventListener("message", messageHandler, false);
+var isActive = true;
 var isNew = true;
 var changed = false;
+var hot;
+var settings = {
+    minRows: 1,
+    minCols: 1,
+    rowHeaders: false,
+    colHeaders: false,
+    filters: true,
+    dropdownMenu: true,
+    //collapsibleColumns: true,
+    hiddenColumns: true,
+    contextMenu: true,
+    manualRowResize: true,
+    manualColumnResize: true,
+    contextMenuCopyPaste: true,
+    copyPaste: true,
+    mergeCells: true,
+    search: true,
+    stretchH: 'all',
+    autoColumnSize: {useHeaders: true},
+    autoRowSize: {syncLimit: 300},
+    width: 1000,
+    height: window.innerHeight - 214,
+    licenseKey: "63ae9-00dfe-0b600-f450d-35624"
+};
 
 $(document).ready(function(){
+    Handsontable.dom.addEvent(document.getElementById("search_field"), 'keyup', function (event) {
+        var search = hot.getPlugin('search');
+        var queryResult = search.query(this.value);
+
+        console.log(queryResult);
+        hot.render();
+    });
+
     $(document).on({
         keyup: function(){
             changed = true;
         }
+    });
+
+    $(window).on({
+        focus: function(){
+            isActive = true;
+            $("#page-cover").fadeOut();
+        },
+        blur: function(){
+            isActive = false;
+            $("#page-cover").fadeIn();
+        }
+    });
+
+    $('#export-csv').on('click', function(e) {
+        hot.getPlugin('exportFile').downloadFile('csv', {filename: $("#table-container").data("text")});
+    });
+
+    $('#toggle-header').on('click', function(e){
+        settings.colHeaders = !settings.colHeaders;
+        settings.rowHeaders = !settings.rowHeaders;
+        hot.updateSettings({
+            "colHeaders": settings.colHeaders,
+            "rowHeaders": settings.rowHeaders
+        });
+        hot.render();
+    });
+
+    $('#add-more').on("click", function(){
+        var data = hot.getData();
+        hot.loadData(data.concat(generateData(100, data[0].length)));
+        changed = true;
     });
 
     $("#close-btn").on("click", function(){
@@ -15,17 +79,7 @@ $(document).ready(function(){
             return;
         }
         if (isNew) {
-            $.ajax({
-                url: "../process.php",
-                type: "POST",
-                data: {
-                    action: "save-app",
-                    data: JSON.stringify($("#table-container").handsontable('getData'))
-                },
-                success: function (res) {
-                    parent.window.postMessage({action: "close-iframe", isChanged: changed, isNew: isNew, time: res}, '*');
-                }
-            });
+            saveApp();
         } else {
             $.ajax({
                 url: "../process.php",
@@ -33,7 +87,7 @@ $(document).ready(function(){
                 data: {
                     action: "save-app",
                     name: $("#table-container").data("name"),
-                    data: JSON.stringify($("#table-container").handsontable('getData'))
+                    data: JSON.stringify(hot.getData())
                 },
                 success: function (res) {
                     parent.window.postMessage({action: "close-iframe", isChanged: changed, isNew: isNew, time: res}, '*');
@@ -41,6 +95,8 @@ $(document).ready(function(){
             });
         }
     });
+
+    setInterval(saveApp, 5 * 60000);
 });
 
 function messageHandler(message){
@@ -49,22 +105,10 @@ function messageHandler(message){
     switch (action){
         case "app-name":
             var name = message.data.name;
+            var text = message.data.text;
             if (name == ""){
                 isNew = true;
-                $("#table-container").handsontable({
-                    data: [["", "", "", "", ""],
-                        ["", "", "", "", ""],
-                        ["", "", "", "", ""],
-                        ["", "", "", "", ""]],
-                    startRows: 4,
-                    startCols: 5,
-                    minRows: 4,
-                    minCols: 5,
-                    rowHeaders: true,
-                    colHeaders: true,
-                    filters: true,
-                    contextMenu: true
-                });
+                hot = new Handsontable(document.getElementById("table-container"), $.extend(settings, {data: generateData(100, 10)}) );
             } else {
                 isNew = false;
                 $.ajax({
@@ -75,15 +119,42 @@ function messageHandler(message){
                         name: name
                     },
                     success: function (res) {
-                        $("#table-container").handsontable({
-                            data: $.parseJSON(res),
-                            rowHeaders: true,
-                            colHeaders: true,
-                            contextMenu: true
-                        }).data("name", name);
+                        hot = new Handsontable(document.getElementById("table-container"), $.extend(settings, {data: $.parseJSON(res)}));
+                        $("#table-container").data({
+                            "name": name,
+                            "text": text
+                        });
                     }
                 });
             }
             break;
     }
+}
+
+function saveApp(){
+    if (isActive) {
+        $.ajax({
+            url: "../process.php",
+            type: "POST",
+            data: {
+                action: "save-app",
+                data: JSON.stringify(hot.getData())
+            },
+            success: function (res) {
+                parent.window.postMessage({action: "close-iframe", isChanged: changed, isNew: isNew, time: res}, '*');
+            }
+        });
+    }
+}
+
+function generateData(rows, cols){
+    var res = [], row = [];
+    for (var i = 0; i < rows; i ++){
+        row = [];
+        for (var j = 0; j < cols; j ++){
+            row.push("");
+        }
+        res.push(row);
+    }
+    return res;
 }
